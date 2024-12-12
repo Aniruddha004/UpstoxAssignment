e29357bddd587e5617755067d9e328304a76c742
aniruddhadeshmukh <aniruddha.deshmukh@bajajfinserv.in>
2024-12-12 14:49:28 +0530

    added total pnl card and rupee string for values

--- app/src/main/java/com/upstox/assignment/ui/adapter/HoldingsAdapter.kt
+++ app/src/main/java/com/upstox/assignment/ui/adapter/HoldingsAdapter.kt
@@ -3,9 +3,11 @@ package com.upstox.assignment.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.upstox.assignment.R
 import com.upstox.assignment.data.model.Holding
 import com.upstox.assignment.databinding.ItemHoldingBinding
 import com.upstox.assignment.utils.roundOffDecimal
+import kotlin.math.absoluteValue
 
 class HoldingsAdapter(private val holdings: List<Holding>) : RecyclerView.Adapter<HoldingsAdapter.HoldingViewHolder>() {
 
@@ -22,11 +24,17 @@ class HoldingsAdapter(private val holdings: List<Holding>) : RecyclerView.Adapte
 
     class HoldingViewHolder(private val binding: ItemHoldingBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(holding: Holding) {
-            val pnl = holding.quantity * (holding.avgPrice - holding.ltp)
+            val pnl = holding.quantity * ( holding.ltp - holding.avgPrice)
             binding.symbol.text = holding.symbol
             binding.quantity.text = holding.quantity.toString()
-            binding.ltp.text = holding.ltp.toString()
-            binding.pnl.text = pnl.roundOffDecimal().toString()
+            binding.ltp.text = "${binding.root.context.getString(R.string.rupee_symbol_c3)} ${holding.ltp.toString()}"
+            if(pnl>0.0){
+                binding.pnl.text = "${binding.root.context.getString(R.string.rupee_symbol_c3)} ${pnl.roundOffDecimal()}"
+                binding.pnl.setTextColor(binding.root.context.resources.getColor(R.color.green))
+            } else {
+                binding.pnl.text = "- ${binding.root.context.getString(R.string.rupee_symbol_c3)} ${pnl.roundOffDecimal()}"
+                binding.pnl.setTextColor(binding.root.context.resources.getColor(R.color.red))
+            }
         }
     }
 }
--- app/src/main/java/com/upstox/assignment/ui/portfolio/HoldingsFragment.kt
+++ app/src/main/java/com/upstox/assignment/ui/portfolio/HoldingsFragment.kt
@@ -9,11 +9,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.upstox.assignment.R
 import com.upstox.assignment.databinding.FragmentHoldingsBinding
 import com.upstox.assignment.ui.adapter.HoldingsAdapter
 import com.upstox.assignment.ui.viewmodel.HoldingsViewModel
 import com.upstox.assignment.ui.viewmodel.HoldingsViewModelFactory
 import com.upstox.assignment.utils.isOnline
+import com.upstox.assignment.utils.roundOffDecimal
+import kotlin.math.absoluteValue
 
 class HoldingsFragment : Fragment() {
 
@@ -43,6 +46,20 @@ class HoldingsFragment : Fragment() {
                 val adapter = HoldingsAdapter(holdingsResponse.data.userHolding)
                 binding.recyclerView.layoutManager = LinearLayoutManager(context)
                 binding.recyclerView.adapter = adapter
+
+                val totalInvestment = holdingsResponse.data.userHolding.sumOf { it.quantity * it.avgPrice }
+                val currentValue = holdingsResponse.data.userHolding.sumOf { it.quantity * it.ltp }
+                val totalPnl = currentValue - totalInvestment
+
+                // Set total PNL to TextView
+                if(totalPnl>0.0){
+                    binding.cardStrip.cardPnlValue.text = "${getString(R.string.rupee_symbol_c3)} ${totalPnl.roundOffDecimal()}"
+                    binding.cardStrip.cardPnlValue.setTextColor(resources.getColor(R.color.green))
+                }
+                else {
+                    binding.cardStrip.cardPnlValue.text = "-${getString(R.string.rupee_symbol_c3)} ${totalPnl.absoluteValue.roundOffDecimal()}"
+                    binding.cardStrip.cardPnlValue.setTextColor(resources.getColor(R.color.green))
+                }
             }
         }
         else {
